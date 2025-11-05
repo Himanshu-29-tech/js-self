4f993691c8b5462e954e0364134c5985f4f52397
Himanshu Yadav ☕️ <himanshuaman92@gmail.com>
2025-11-05 20:17:44 +0000

    conversion of data type

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,32 @@
+ let score = "himanshu"
+
+ console.log(typeof score);
+ console.log(typeof(score));
+
+let valueInNumber = Number(score) //change the string value to int
+ console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+//"33" => 33
+// "33abc" =< NaN
+// true => 1; false => 0
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+let isLoggedIn1 =""
+
+let booleanIsLoggedIn1 = Boolean(isLoggedIn1)
+console.log(booleanIsLoggedIn1);
+
+//1 => true; = => false
+// "" => false
+// "himanshu" => true
+
+let someNumber = 33 
+
+let stringNumber = string(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
