b268d2a1e09332448a07db5b15fa5727368d380c
Himanshu Yadav ☕️ <himanshuaman92@gmail.com>
2025-11-05 21:59:55 +0000

    comparision

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,25 @@
+ //comparision of same data type
+ console.log(2 > 1);
+ console.log(2 >= 1);
+ console.log(2 < 1);
+ console.log(2 == 1)
+ console.log( 2 != 1);
+
+// comparision of string to digit 
+ console.log("2" > 1);
+ console.log("02" >= 1);
+
+ console.log(null > 0);
+ console.log(null == 0); 
+ console.log(null >= 0);
+ //null converted to 0 automatically
+
+ console.log(undefined == 0);
+ console.log(undefined > 0);
+ console.log(undefined < 0);
+
+ //we avoid this type of code 
+
+ // === check value strictly
+
+ console.log("2" === 2);
