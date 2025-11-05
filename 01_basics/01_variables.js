e64171dc489591df4d9863e4c3d0c86ffdaeacc8
Himanshu Yadav ☕️ <himanshuaman92@gmail.com>
2025-11-05 19:40:22 +0000

    datatpes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,28 @@
+const accountId = 12945
+// const to bhai change nhi ho sakta
+
+let accountEmail = "himanshuji.com"
+var accountPassword = "12345"
+// variable likhne ke do tarika let&var
+accoountCity = "Delhi"
+let accountstate;
+// javascript undefined hai ji
+
+
+// chalo change karte hai
+
+// accountId = 2// not allowed
+// shotcut-log+enter
+accountEmail = "hji@29.com"
+accountPassword = "45673"
+accountcity = "Rajasthan"
+
+
+console.log(accountId); 
+console.log(accountEmail)
+
+
+// sabko baar baar kyu likhna eshliye dictionary bnaate hai ji
+
+console.table([accountId,accountEmail,accountPassword,accoountCity])
+
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,33 @@
+ "use strict;"// treat all js code as newer version
+
+ 
+// alert(3 + 3) // we are using nodejs, not browser
+
+console.log(3 + 3)//code readebility should be high
+ 
+console.log("Himanshu")
+
+
+let name = "Himanshu" //string value
+let age = 18 //int
+let isLoggedIn = false //boolean value
+
+
+//number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+//null => standalone value 
+// undefined => 
+// symbol => unique
+
+
+//object
+
+console.log(typeof "himanshu");
+console.log(typeof age);
+console.log(typeof null); // object
+console.log(typeof undefined); //undefined
+
+
+
