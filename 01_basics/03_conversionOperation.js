d061946d0c6497573e37acf28579f6751c631da8
Himanshu Yadav ☕️ <himanshuaman92@gmail.com>
2025-11-05 22:05:18 +0000

    conversion continue

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -25,8 +25,47 @@ console.log(booleanIsLoggedIn1);
 // "" => false
 // "himanshu" => true
 
-let someNumber = 33 
+// let someNumber = 33 
 
-let stringNumber = string(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// let stringNumber = string(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// operations//
+
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**3);
+console.log(2/3);
+console.log(2%3);
+
+let str1 = "hello"
+let str2 = "himanshu"
+
+let str3 = str1 + str2
+console.log(str3);
+
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1"+ 2 + 2);
+console.log(1+ 2 + "2");
+
+console.log(true); // -->true
+console.log(+true);// -->1
+console.log(+""); // -->0
+
+let num1,num2,num3
+
+num1 = num2 =  num3 = 2 + 2
+
+let gamecounter = 100
+++gamecounter;
+console.log(gamecounter);
+//go through documentation of mdn increment operator
+
