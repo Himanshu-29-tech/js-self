fc192f07b3ea4e3d1509661a4f3cd8b60a05b174
Himanshu Yadav ☕️ <himanshuaman92@gmail.com>
2026-01-07 16:37:37 +0000

    object

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,13 @@
+//singleton
+//Obect.create
+//object literals
+
+const jsUser = {
+    name: "Himanshu",
+    age: 18,
+    location: "Haryana",
+    email: "himuji@gmail.com",
+    isLoggedIn: false,
+    lastLoginDays: ["Monday","Saturday"]
+}
+
