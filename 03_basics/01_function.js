1d256ac0f2da633e2031937bfc78b94380364d37
Himanshu Yadav ☕️ <himanshuaman92@gmail.com>
2026-01-08 07:51:16 +0000

    function

--- 03_basics/01_function.js
+++ 03_basics/01_function.js
@@ -57,3 +57,35 @@ function loginUserMessage(username){
 
 console.log(loginUserMessage("Himanshu"))
 console.log(loginUserMessage())//if value is not given then undefined will be print
+
+
+function calculatecartprice(...num1){
+    return num1
+}
+
+console.log(calculatecartprice(200,400,500))
+
+const user = {
+    username: "Himanshu",
+    price: 199
+}
+
+function handleObject(anyobject){
+    console.log(`username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+//handleObject(user)
+
+handleObject({
+    username: "Sam",
+    price: 399
+})
+
+const myNewArray = [200,400,100,600]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200,400,500,1000]));
+
