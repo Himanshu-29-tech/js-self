8e90f6d2d00dcd1ff8c446ee11c252040b429110
Himanshu Yadav ☕️ <himanshuaman92@gmail.com>
2026-07-17 06:51:54 +0530

    Enhance prototypes with custom methods and examples
    Added prototype methods for String and Array, and demonstrated usage with examples.

--- 05_prototype.js
+++ 05_prototype.js
@@ -1,1 +1,74 @@
+// let myName = "himanshu    "
+// let mychannel = "Water    "
 
+// console.log(myName.trim().length); // output is 8
+// console.log(myName.truelength); // output is 8
+
+
+
+let myHeroes = ["Raju", "Kaju"]
+// object
+let heroPower = {
+  Raju : "Travel",
+  kaju : "Foody",
+
+  getheroPower: function(){
+    console.log(`Food is favourite of ${this.kaju}`);
+  }
+}
+
+Object.prototype.himanshu = function(){
+  console.log(`himanshu is present in all objects`);
+}
+
+Array.prototype.heyhimanshu = function(){
+  console.log(`himanshu says hello`);
+}
+
+
+//heroPower.himanshu()
+myHeroes.himanshu()
+myHeroes.heyhimanshu()
+//heroPower.heyhimanshu() // no acess of heyhimanshu()
+
+
+// inheritance
+
+const User = {
+  name: "Water",
+  email: "water@google.com"
+}
+
+const Teacher = {
+  makeVideo : true
+}
+
+const TeachingSupport = {
+  isAvailable: false
+}
+
+const TASupport = {
+   makeAssignment: 'JS assignment',
+   fullTime: true,
+   __proto__: TeachingSupport
+}
+
+Teacher.__proto__ = User
+
+// modern syntax
+
+Object.setPrototypeOf(TeachingSupport, Teacher)
+
+
+let anotherUsername = "Hamaurwo   "
+
+String.prototype.trueLength = function(){
+  console.log(`${this}`);
+  console.log(`${this.name}`);
+  console.log(`True length is: ${this.trim().length}`);
+
+}
+
+anotherUsername.trueLength()
+"Himanshu".trueLength()
+"iceTea".trueLength()
