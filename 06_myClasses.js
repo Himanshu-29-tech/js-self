943dbe7ec6f600fa46ef357471de0ce9575e5d13
Himanshu Yadav ☕️ <himanshuaman92@gmail.com>
2026-07-17 06:52:26 +0530

    Implement User class and User2 constructor function

--- 06_myClasses.js
+++ 06_myClasses.js
@@ -1,1 +1,42 @@
+// ES6 
 
+class User {
+  constructor(username, email, password){
+    this.username = username;
+    this.email = email;
+    this.password = password
+  }
+
+  encryptPassword(){
+    return `${this.password}abc`
+  }
+  changeUsername(){
+    return `${this.username.toUpperCase()}`
+  }
+}
+
+const water = new User("Water" , "water@gmail.com", "123")
+console.log(water.encryptPassword());
+console.log(water.changeUsername());
+
+
+// behind the scene
+
+function User2 (username, email, password){
+    this.username = username;
+    this.email = email;
+    this.password = password
+  }
+
+  User2.prototype.encryptPassword = function(){
+    return `${this.password}abc`
+  }
+
+  User2.prototype.changeUsername = function(){
+    return `${this.toUpperCase()}`
+  }
+
+  const tea =  new User2("tea","tea@google.com","567")
+
+  console.log(tea.encryptPassword());
+  console.log(tea.changeUsername());
