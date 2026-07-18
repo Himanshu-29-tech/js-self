7e802005454aef07ad55e6848dc260903f809321
Himanshu Yadav ☕️ <himanshuaman92@gmail.com>
2026-07-18 08:54:43 +0530

    Implement getter and setter for email and password

--- 07_getter_setter.js
+++ 07_getter_setter.js
@@ -1,1 +1,27 @@
+class User {
+  constructor(email, password){
+    this.email = email;
+    this.password = password
+  }
 
+  get email(){
+    return this._email.toUpperCase()
+  }
+
+  set email(value){
+     this._email = value
+  }
+
+  get password(){ // if you want to get(access) the value out of the class
+    return this._password.toUpperCase()
+    
+  }
+  set password(value){  // if you want to set the value inside the class
+    this._password = value.toUpperCase()
+
+  }
+}
+
+
+const himanshu = new User("h@himanshu.ai", "abc")
+console.log(himanshu.email);
