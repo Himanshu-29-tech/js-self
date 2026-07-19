869159f93a0d1dfcf28903a75a7c0b8f92d5b353
Himanshu Yadav ☕️ <himanshuaman92@gmail.com>
2026-07-19 13:28:39 +0530

    Implement static method createId in User class
    Added static method createId to User class and demonstrated its usage in instances.

--- 010_staticprop.js
+++ 010_staticprop.js
@@ -1,1 +1,27 @@
+class User {
+  constructor(username) {
+    this.username = username
+  }
 
+  logMe(){
+    console.log(`Username: ${this.username}`);
+  }
+
+   static createId(){   // use of static stop the methods that are going to be used 
+    return `123`
+  }
+}
+
+const himanshu = new User("himanshu")
+console.log(himanshu.createId())
+
+
+class Teacher extends User {
+  constructor(username,email){
+    super(username)
+    this.email = email
+  }
+}
+
+const iphone = new Teacher("iphone", "i@phone.com")
+console.log(iphone.createId());
